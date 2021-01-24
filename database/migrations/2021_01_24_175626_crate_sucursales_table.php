188082c4d5211ffb1fbb46d437dfca53ea1f7181
ANATOG <anitatorrez1924@gmail.com>
2021-01-24 12:20:27 -0600

    migraciones a la mitad

---
+++ database/migrations/2021_01_24_175626_crate_sucursales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrateSucursalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sucursales', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->boolean('condicion')->default(1);
+            $table->string('direccion', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sucursales');
+    }
+}
